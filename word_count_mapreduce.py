e5fae0b998d8caf230aae0a3fe474b3bda8b6967
jb892 <jack-jubo@hotmail.com>
2017-05-14 04:29:44 +0100

    Realse-V0.1

--- word_count_mapreduce.py
+++ word_count_mapreduce.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
-string_t = 'Hello my friend! How are you? Would you like to have a cup of tea?'
+string_t = "Hello my friend! How are you? Would you like to have a cup of tea? Could you please tell a story about yourself?"
+
+print "Original String: \n", string_t, "\n"
 
 def filter_specail(s, special_set = ['?', '!', ',', '\\', '/']):
 	for _, item in enumerate(special_set):
@@ -10,14 +12,14 @@ def filter_specail(s, special_set = ['?', '!', ',', '\\', '/']):
 new_s = map(filter_specail, [string_t])
 new_s = map(lambda x: x.lower(), new_s)
 new_s = map(lambda x: x.split(), new_s)
-print new_s
+print "After filter special character(lower() and split()): \n", new_s, "\n"
 
 def shuffle(s):
 	return {s:1}
 
 new_s = map(shuffle, new_s[0])
-print new_s
+print "After shuffle: \n", new_s, "\n"
 
 count_dict = reduce(lambda x, y: Counter(x) + Counter(y), new_s)
 
-print count_dict
+print "After 'reduce' we get final result dictionary: \n", count_dict
